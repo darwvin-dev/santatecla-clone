0fe1a89ce046ebb847693ef3e8788313556974c2
Darwvin <darwvin@hotmail.com>
2025-08-23 18:44:39 +0330

    builded

--- src/app/(site)/[locale]/components/apartments/PropertyMap.tsx
+++ src/app/(site)/[locale]/components/apartments/PropertyMap.tsx
@@ -6,37 +6,32 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 
-function ChangeView({
-  coords,
-  zoom = 12,
-}: {
+interface MapProps {
   coords: [number, number];
   zoom?: number;
-}) {
+}
+
+function ChangeView({ coords, zoom = 12 }: MapProps) {
   const map = useMap();
   map.setView(coords, zoom);
   return null;
 }
 
-export default function Map() {
-  const [geoData] = useState({ lat: 64.536634, lng: 16.779852 });
-  const center = useMemo<[number, number]>(
-    () => [geoData.lat, geoData.lng],
-    [geoData.lat, geoData.lng]
-  );
+export default function PropertyMapClient({ coords, zoom = 12 }: MapProps) {
+  const center = useMemo<[number, number]>(() => coords, [coords]);
 
   return (
     <div
       style={{
-        height: 300,
-        width: "85%",
-        margin: "auto",
+        height: 360,
+        width: "100%",
+        borderRadius: 12,
         overflow: "hidden",
       }}
     >
       <MapContainer
         center={center}
-        zoom={12}
+        zoom={zoom}
         style={{ height: "100%", width: "100%" }}
         scrollWheelZoom={false}
       >
@@ -45,7 +40,7 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         />
         <Marker position={center} />
-        <ChangeView coords={center} zoom={12} />
+        <ChangeView coords={center} zoom={zoom} />
       </MapContainer>
     </div>
   );
